328c084d5a4c15ba3928b2d6649ed4ed2ebc0862
salo-saldarriagav <123768554+salo-saldarriagav@users.noreply.github.com>
2023-02-10 14:01:59 -0500

    imagenes

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const cardAdj = [
+    {
+      name: "salome1",
+      img: "imagenes/salome1.png"
+    },
+    {
+      name: "salome2",
+      img: "imagenes/salome2.png"
+    },
+    {
+      name: "salome3",
+      img: "imagenes/salome3.png"
+    },
+    {
+      name: "salome4",
+      img: "imagenes/salome4.png"
+    },
+    {
+      name: "salome5",
+      img: "imagenes/salome5.png"
+    },
+    {
+      name: "salome6",
+      img: "imagenes/salome6.png"
+    }
+  ];
+});
