c0a9cbc8c50e15294dc44d010415c79b5d6f5152
salo-saldarriagav <123768554+salo-saldarriagav@users.noreply.github.com>
2023-02-13 12:33:30 -0500

    cuadricula

--- app.js
+++ app.js
@@ -25,4 +25,9 @@ document.addEventListener("DOMContentLoaded", () => {
       img: "imagenes/salome6.png"
     }
   ];
+  const cuadricula = document.querySelector(".cuadricula");
+  const resultado = document.querySelector("#resultado");
+  var cartasEscogidas = [];
+  var cartasEscogidasId = [];
+  var cartasGanadas = [];
 });
