e241cf9cd9d39c7700009ab1a94baa77dcb3fd4e
salo-saldarriagav <123768554+salo-saldarriagav@users.noreply.github.com>
2023-03-10 13:55:06 -0500

    voltear carta

--- app.js
+++ app.js
@@ -23,6 +23,30 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       name: "salome6",
       img: "imagenes/salome6.png"
+    },
+    {
+      name: "salome1",
+      img: "imagenes/salome1.png"
+    },
+    {
+      name: "salome2",
+      img: "imagenes/salome2.png"
+    },
+    {
+      name: "salome3",
+      img: "imagenes/salome3.png"
+    },
+    {
+      name: "salome4",
+      img: "imagenes/salome4.png"
+    },
+    {
+      name: "salome5",
+      img: "imagenes/salome5.png"
+    },
+    {
+      name: "salome6",
+      img: "imagenes/salome6.png"
     }
   ];
   const cuadricula = document.querySelector(".cuadricula");
@@ -35,10 +59,22 @@ document.addEventListener("DOMContentLoaded", () => {
     for (let i = 0; i < cardAdj.length; i++) {
       var carta = document.createElement("img");
 
-      carta.setAttribute("src", "images/reverso.png");
+      carta.setAttribute("src", "imagenes/reverso.png");
       carta.setAttribute("data-id", i);
       carta.addEventListener("click", voltearCarta);
       cuadricula.appendChild(carta);
     }
   }
+
+  function voltearCarta() {
+    var cardId = this.getAttribute("data-id");
+    cartasEscogidas.push(cardAdj[cardId].name);
+    cartasEscogidasId.push(cardId);
+    this.setAttribute("src", cardAdj[cardId].img);
+    if (cartasEscogidas.length === 2) {
+      setTimeout(verificarpareja, 1000);
+    }
+  }
+
+  crearTablero();
 });
